50038097a2dc80ca8da71768a019c50948d34718
nhannhanfb <86229195+nhannhanfb@users.noreply.github.com>
2021-12-28 08:53:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,16 @@
 const CONFIG = {
     titleWeb: "Gửi Bé Tuyên Iu Dấu Của A",
     introTitle: 'gửi bé iu ❤Dương Anh Thủy Tuyên❤ của a',
-    introDesc: `thật là may mắn khi anh có được em 🥰đó là điều tuyệt với nhất với anh❤',
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰😍',
-    desc: 'A quá yêu em gòii em có yêu a humm 😍❤',
-    btnYes: 'Yêu lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'mãi yêu anh bae oii❤  <33333333',
-    mess: 'Anh biết mà 😆🥰. Yêu Em nhiều lắm bae oiii nhiều lắm luôn😘😘'
+    introDesc : A rất yêu em bae ơii a muốn được gặp em 
+ôm em và bảo vệ em nữa em oii ` ,
+    btnIntro : '^^ HiHi ^^' ,
+    title : 'A iu em nhiều lắm bae ơiii yêu nhiềuu lắm 🥰' ,
+    desc : 'Bae iu dấu có yêu anh hong nè❤? ' ,
+    btn Có : 'Yêu nhắmm❤ <33' ,
+    btnNo : 'Không nha: 3' ,
+    question : 'Trên thế giới hơn 7 tỉ lệ người sao em lại yêu enh nè <3' ,
+    btnReply : 'Gửi cho bạn <3' ,
+    reply : 'Yêu Anh nhấtt uônn❤<33333333' ,
+    mess : 'Anh biết mà😆🥰. Yêu Em nhiều lắm bae oiii nhiều lắm 😘😘 ' ,
+    messLink : 'https://www.facebook.com/profile.php?id=100023920961176' 
+}

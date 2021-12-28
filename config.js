8abc11ce229cd429ee5e9f85fe70e8a4a2a95cf8
nhannhanfb <86229195+nhannhanfb@users.noreply.github.com>
2021-12-28 08:32:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,14 @@
 const CONFIG = {
     titleWeb: "Gửi Bé Tuyên Iu Dấu Của A",
-    introTitle: 'Dương Anh Thủy Tuyên',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'gửi bÉ iu ❤Dương Anh Thủy Tuyên❤ của a',
+    introDesc: `thật là may mắn khi anh có được em 🥰
+đó là điều tuyệt với nhất với anh ❤
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰😍',
+    desc: 'A quá yêu em gòii em có yêu a humm 😍❤',
+    btnYes: 'Yêu lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    reply: 'mãi yêu anh bae oii❤  <33333333',
+    mess: 'Anh biết mà 😆🥰. Yêu Em nhiều lắm bae oiii nhiều lắm luôn😘😘'

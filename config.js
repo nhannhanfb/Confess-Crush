cb8a21d9bdbfc43dc33b2f793f76349969dcd59e
nhannhanfb <86229195+nhannhanfb@users.noreply.github.com>
2021-12-28 09:00:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const CONFIG = {
     titleWeb: "Gửi Bé Tuyên Iu Dấu Của A",
     introTitle: 'gửi bé iu ❤Dương Anh Thủy Tuyên❤ của a',
-    introDesc : A rất yêu em bae ơii a muốn được gặp em 
-ôm em và bảo vệ em nữa em oii ` ,
+    introDesc : A rất yêu em bae ơii a muốn được gặp em ôm em và bảo vệ em nữa em oii ` ,
     btnIntro : '^^ HiHi ^^' ,
-    title : 'A iu em nhiều lắm bae ơiii yêu nhiềuu lắm 🥰' ,
+    title : 'A iu em nhiều lắm bae ơiii yêu nhiềuu lắm 🥰 ' ,
     desc : 'Bae iu dấu có yêu anh hong nè❤? ' ,
     btn Có : 'Yêu nhắmm❤ <33' ,
     btnNo : 'Không nha: 3' ,
@@ -13,4 +12,3 @@ const CONFIG = {
     reply : 'Yêu Anh nhấtt uônn❤<33333333' ,
     mess : 'Anh biết mà😆🥰. Yêu Em nhiều lắm bae oiii nhiều lắm 😘😘 ' ,
     messLink : 'https://www.facebook.com/profile.php?id=100023920961176' 
-}
